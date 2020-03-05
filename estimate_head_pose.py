0ba4eeea259a480d41f6a46fd6ab8d91a1459f41
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2020-03-05 11:51:32 +0600

    add pupil detection and angle estimation

--- estimate_head_pose.py
+++ estimate_head_pose.py
@@ -36,12 +36,81 @@ parser.add_argument("--cam", type=int, default=None,
 args = parser.parse_args()
 
 
+def sharpen(gray):
+    blurred = gray.copy()
+    cv2.GaussianBlur(gray, (5, 5), 0.8, blurred)
+    sharpened = gray.copy()
+    cv2.addWeighted(gray, 1.5, blurred, 0.5, 0, sharpened)
+    return sharpened
+
+
 def get_face(detector, img_queue, box_queue):
     """Get face from image queue. This function is used for multiprocessing"""
     while True:
         image = img_queue.get()
         box = detector.extract_cnn_facebox(image)
         box_queue.put(box)
+
+
+def adjust_angle(eye_frame_part, x, y):
+    adjusted_x = x - eye_frame_part.shape[0] / 2
+    adjusted_y = y - eye_frame_part.shape[1] / 2
+
+    proportion_x = adjusted_x / eye_frame_part.shape[0]
+    proportion_y = adjusted_y / eye_frame_part.shape[1]
+
+    vertical_max_angle = 75
+    horizontal_max_angle = 75
+
+    return proportion_x * horizontal_max_angle, proportion_y * vertical_max_angle
+
+
+def process_eyes(frame, landmarks):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = sharpen(gray)
+
+    left_eye = gray[int(landmarks[38][1]) - 7: int(landmarks[41][1]) + 7, int(landmarks[36][0]) - 7: int(landmarks[39][0]) + 7]
+    right_eye = gray[int(landmarks[44][1]) - 7: int(landmarks[46][1]) + 7, int(landmarks[42][0]) - 7: int(landmarks[45][0]) + 7]
+
+    eyes = [left_eye, right_eye]
+
+    centers = []
+    origins = [[int(landmarks[36][0]), int(landmarks[38][1])], [int(landmarks[42][0]), int(landmarks[44][1])]]
+
+    for eye, origin in zip(eyes, origins):
+        circles = cv2.HoughCircles(eye, cv2.HOUGH_GRADIENT, 1, eye.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=30)
+        eye_center = (eye.shape[0] / 2, eye.shape[1] / 2)
+        if circles is None:
+            # no circles were detected on either or both eyes
+            # so return prematurely
+            return {}
+        circles = np.uint16(np.around(circles))
+        needed_circles = circles[0, :].tolist()
+        needed_circles.sort(key=lambda circle: math.sqrt((eye_center[0] - circle[0])**2 + (eye_center[1] - circle[1])**2))
+        the_circle = needed_circles[0]
+        # simplePutText(eye, str(len(circles)))
+        # for i in circles[0, :]:
+        #     cv2.circle(eye, (i[0], i[1]), i[2], (255, 255, 255), 2)
+        angles = adjust_angle(eye, the_circle[0], the_circle[1])
+        cv2.circle(frame, (the_circle[0] + origin[0], the_circle[1] + origin[1]), 2, (255, 255, 255))
+        centers.append(angles)
+
+    centers = {'right': centers[0], 'left': centers[1]}
+    return centers
+
+
+def angle_to_radian(angle_degrees):
+    return math.pi * angle_degrees / 180
+
+
+def cvt_to_radians(eye_angles):
+    right = eye_angles['right']
+    eye_angles['right'] = (angle_to_radian(right[0]), angle_to_radian(right[1]))
+    
+    left = eye_angles['left']
+    eye_angles['left'] = (angle_to_radian(left[0]), angle_to_radian(left[1]))
+
+    return eye_angles
 
 
 def main():
@@ -133,7 +202,8 @@ def main():
             pixel_distance = int(math.sqrt((right_corner[0] - left_corner[0]) ** 2 + (right_corner[1] - left_corner[1]) ** 2))
             estimated_distance = (real_width * focal_length) / pixel_distance
 
-            cv2.putText(frame, str(round(estimated_distance, 2)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0))
+            cv2.putText(frame, str(round(estimated_distance, 2)), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0))
+
 
             # Uncomment following line to show facebox.
             # mark_detector.draw_box(frame, [facebox])
@@ -150,15 +220,21 @@ def main():
             steady_pose = np.reshape(steady_pose, (-1, 3))
 
             # Uncomment following line to draw pose annotation on frame.
-            pose_estimator.draw_annotation_box(
-                 frame, pose[0], pose[1], color=(255, 128, 128))
+            # pose_estimator.draw_annotation_box(
+            #     frame, pose[0], pose[1], color=(255, 128, 128))
 
             # Uncomment following line to draw stabile pose annotation on frame.
             # pose_estimator.draw_annotation_box(
             #    frame, steady_pose[0], steady_pose[1], color=(128, 255, 128))
 
             # Uncomment following line to draw head axes on frame.
+            print(steady_pose[1])
             pose_estimator.draw_axes(frame, steady_pose[0], steady_pose[1])
+
+            angles = process_eyes(frame, marks)
+            if bool(angles) is True:
+                # print(angles)
+                angles = cvt_to_radians(angles)
 
         # Show preview.
         cv2.imshow("Preview", frame)
